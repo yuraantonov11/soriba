66a264f8556c6a41a47e6cebf1443df9d08fe495
yuraantonov11 <yuraantonov11@gmail.com>
2018-08-14 21:58:56 +0300

    product fix

--- controllers/home.js
+++ controllers/home.js
@@ -1,9 +1,24 @@
+const Product = require('../models/Product');
+const Category = require('../models/Category');
 /**
  * GET /
  * Home page.
  */
 exports.index = (req, res) => {
-  res.render('home', {
-    title: 'Home'
-  });
+    Product
+        .find({})
+        .exec((err, products) => {
+            if (err) return res.send(err);
+            // this will log all of the users with each of their posts
+            Category.find({})
+                .exec((err, categories) => {
+                    if (err) console.log(err);
+                    // this will log all of the users with each of their posts
+                    res.render('home', {
+                        title: 'Demo',
+                        products,
+                        categories
+                    });
+                });
+        });
 };
