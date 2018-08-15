c388dc1ec1b715f2f9d6949013d7d216f477f6d8
yuraantonov11 <yuraantonov11@gmail.com>
2018-08-15 22:50:06 +0300

    add modal data finish

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,14 @@
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        const reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('.preview-image').attr('src', e.target.result);
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
 $(document).ready(() => {
     const requestUrl = window.location.origin;
     // categories
@@ -26,28 +37,31 @@ $(document).ready(() => {
     $('#product-review-modal').on('show.bs.modal', () => {
         const modal = $('#product-review-modal');
         const formData = $('form').serializeArray();
+
+        const showChoice = formData.some(element => element.name === 'choice');
+        modal.find('.bestProduct').toggle(showChoice);
+
         formData.forEach((element) => {
-            if (element.name === 'features') {
+            if (element.name === 'features' && element.value !== '') {
                 modal.find(`.${element.name}`).append(`<li>${element.value}</li>`);
+            } else if (element.name === 'link') {
+                modal.find(`.${element.name}`).attr('href', element.value);
+            } else if (element.name === 'rating') {
+                const stars = modal.find('.rating>span.fa-star');
+                stars.each(function (index) {
+                    $(this).toggleClass('checked', index < element.value);
+                });
+            } else {
+                modal.find(`.${element.name}:empty`).text(element.value);
             }
-            modal.find(`.${element.name}:empty`).text(element.value);
         });
     });
-});
-
-
-function readURL(input) {
-    if (input.files && input.files[0]) {
-        const reader = new FileReader();
-
-        reader.onload = function (e) {
-            $('.preview-image').attr('src', e.target.result);
-        };
-
-        reader.readAsDataURL(input.files[0]);
-    }
-}
-
-$('#imgInp').change(function () {
-    readURL(this);
+    $('#product-review-modal').on('hidden.bs.modal', () => {
+        const modal = $('#product-review-modal');
+        console.log(modal.find('.features'));
+        modal.find('.features').empty();
+    });
+    $('#imgInp').change(function () {
+        readURL(this);
+    });
 });
